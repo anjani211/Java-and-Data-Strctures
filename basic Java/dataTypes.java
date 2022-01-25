a3b164aad7982222d7d0db97a176d9c8fd4f8088
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-01-25 17:56:29 +0530

    Add files via upload

---
+++ basic Java/dataTypes.java
@@ -0,0 +1,16 @@
+public class dataTypes{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 2;
+        String h = "anjani";
+        double k = 22;
+        long s = 33;
+        byte p = 111;
+        System.out.println(a);
+        System.out.println(h);
+        System.out.println(k);
+        System.out.println(p);
+
+        
+    }
+}
