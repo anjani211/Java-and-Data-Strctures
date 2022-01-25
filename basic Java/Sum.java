7f4351ab7eb1ba1ec132875f3af6d6ad5b225d20
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-01-25 17:59:32 +0530

    Add files via upload

---
+++ basic Java/Sum.java
@@ -0,0 +1,12 @@
+// here we will make a program which gives output as a sum of three numbers
+public class Sum {
+    
+    public static void main(String[] args) {
+        
+        int a = 2;
+        int b = 4;
+        int c = 10;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
