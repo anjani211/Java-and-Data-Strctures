dd89e76ded1424d258fbc26136a279631f1da3fa
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-01-25 22:11:04 +0530

    Add files via upload

---
+++ basic Java/Literals.java
@@ -0,0 +1,17 @@
+// A constant value which can be assigned to the variable is called as a Literals
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 33;
+        char ch = 'A';
+        float f1 = 5.2f;
+        double d1  = 4.33d;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+
+ 
+        
+    }
+    
+}
