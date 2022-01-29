55e1fe35a1d368e8656d9f6962581110ff1aad4d
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-01-29 14:51:45 +0530

    Add files via upload

---
+++ basic Java/loops.java
@@ -0,0 +1,25 @@
+public class loops {
+    public static void main(String[] args) {
+        /* Syntax for for loop:
+        for(initialisation;conditioin;increment/decrement){
+            body
+        }
+        */
+        // Printing numbers 1 to 5
+
+        // for(int i=1;i<=5;i+=1){
+        //     System.out.println(i);
+        // }
+        /* while loop syntax
+        while(condition){
+            body
+        }
+        */
+        int j = 1;
+        while(j<5){
+            System.out.println(j);
+            j += 1;
+        }
+    }
+    
+}
