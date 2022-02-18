f45643272aee723669c62a5f7db5f809724ff271
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-02-18 23:12:46 +0530

    Add files via upload

---
+++ DataStructures/arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 33;
+        arr[1] = 22;
+        arr[2] = 34;
+        arr[3] = 45;
+        arr[4] = 55;
+        
+        System.out.println(arr.length);
+        for(int i = 0; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+    
+}
